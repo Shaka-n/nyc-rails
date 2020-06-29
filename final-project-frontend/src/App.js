d2c1e92d5ba743bd4596c6673532d8fa3e33df93
Garen Sahagian <g.s.sahagian@gmail.com>
2020-06-29 11:46:25 -0400

    Hiding key

--- final-project-frontend/src/App.js
+++ final-project-frontend/src/App.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const ProtoBuf = require('protobufjs')
+const request = require('request')
+const gtfsRB = require('gtfs-rb').transit_realtime
+
+const dotenv = require("dotenv");
+dotenv.config();
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const requestSettings = {
+  method: 'GET',
+  url: 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',
+  encoding: null,
+  headers: { 
+    "Content-Type": "application/x-protobuf",
+    "Accept": "application/x-protobuf",
+    "x-api-key": API_KEY }
+}
 
 function App() {
+
+const [stops, setStops] = useState([])
+
+  useEffect(()=>{
+    // request(requestSettings, (error, response, body) => {
+    //   if (!error && response.statusCode === 200) {
+    //     ProtoBuf.load(['services/config/nyct-subway.proto', 'services/config/gtfs-realtime.proto']).then((root) => {
+    //       console.log(root.lookupType('FeedMessage').decode(body))
+    //     })
+    //   } else {
+    //     console.log(`Error: ${error}, Status Code: ${response.statusCode}`)
+    //   }
+    // })
+
+    request(requestSettings, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        var feed = gtfsRB.FeedMessage.decode(body)
+        feed.entity.forEach(function(entity) {
+          console.log(entity)
+          // if (entity.trip_update) {
+          //   console.log(entity.trip_update);
+          // }
+        })
+      }
+    })
+
+})
+
   return (
     <div className="App">
       <header className="App-header">
