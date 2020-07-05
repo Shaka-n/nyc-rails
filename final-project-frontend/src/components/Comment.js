0bee6c118c46e99bebab74667328b009b2a2bf64
Garen Sahagian <g.s.sahagian@gmail.com>
2020-07-05 10:54:39 -0400

    working on comment components

---
+++ final-project-frontend/src/components/Comment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Comment = (props) =>{
+return(
+    <div>
+        {/* <p>{props.user} said:</p>
+        <p>{props.body}</p>
+        <p>at {props.created_at}</p> */}
+    </div>
+)
+}
+
+export default Comment
