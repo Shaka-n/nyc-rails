8a198b8cb84816d2843bca10a080ca3dfd40ee71
Garen Sahagian <g.s.sahagian@gmail.com>
2020-07-05 12:25:16 -0400

    found doubling bug. still need to move on to comments.

--- final-project-frontend/src/components/LineContainer.js
+++ final-project-frontend/src/components/LineContainer.js
@@ -10,9 +10,13 @@ const renderStationSummary = (targetStation) =>{
     // Find the paired stops at each station
     let pairedStops
         pairedStops = props.currentSchedules.filter(stop => stop.stationName===targetStation.stationName)
-
-    // Render each stop in a StationSummary component
-    console.log("Paired stops", pairedStops)
+    // makeStopPairs(props.currentSchedules)
+    // Render each stop in a StationSummary component.
+    // XXX
+    // THIS IS WHERE THE DOUBLING UP IS HAPPENING. BOTH NORTH AND SOUTHBOUND TRIPS END UP IN PAIRS.
+    //  NEED TO MAKE SOME SORT OF REDUCER TO SKIP PAST AN ENTRY IF THERE IS ALREADY A PAIR CONTAINING IT.
+    // XXX
+    // console.log("Paired stops", pairedStops)
     return <StationSummary 
             stops={pairedStops}
             // name={targetStation.stationName} 
@@ -20,6 +24,32 @@ const renderStationSummary = (targetStation) =>{
             // nextArrival={targetStation.nextArrival}
             />
         
+}
+const makeStopPairs = (schedule) =>{
+    // METHOD 1
+    let pairedStopsSchedule =[]
+    
+    for(let i = 0;i>schedule.length;i++){
+        let j = i + 1
+        if(schedule[i].stationName == schedule[j].stationName){
+            pairedStopsSchedule.push({d1:schedule[i], d2:schedule[j]})
+            i++
+        }
+    }
+    console.log(pairedStopsSchedule)
+    // METHOD 2
+    const reducer = (pairedSchedule, stop) =>{
+        if(pairedSchedule.last.d1.stationName === stop.stationName){
+            return pairedSchedule.last.d2 = stop
+        }else{
+            return pairedSchedule.push({d1:stop})
+        }
+    }
+
+    const pairedSchedule = schedule.reduce(stop =>{
+        reducer(stop)
+    })
+    console.log(pairedSchedule)
 }
 
     return(
