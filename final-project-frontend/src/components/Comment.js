3a11d50bbc53e41f22773d08ede576154ebdca4a
Garen Sahagian <g.s.sahagian@gmail.com>
2020-07-05 21:47:25 -0400

    Got a service alert ticker halfway working. Just need to make the text not wrap at all. Also got a delete buton mostly in theree. Just need to make a fetch delete

--- final-project-frontend/src/components/Comment.js
+++ final-project-frontend/src/components/Comment.js
@@ -5,6 +5,7 @@ return(
     <div>
         {/* <p>{props.user} said:</p> */}
         <p>{props.commentBody}</p>
+        <button onClick={() => props.deleteComment(props.commentId)}>X</button>
         {/* <p>at {props.created_at}</p> */}
     </div>
 )
