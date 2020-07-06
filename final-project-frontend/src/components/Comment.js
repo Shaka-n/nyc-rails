a0bacc8c0c9149261f6e10852c5db9c7e3bc1042
Garen Sahagian <g.s.sahagian@gmail.com>
2020-07-06 10:12:58 -0400

    testing merge to master

--- final-project-frontend/src/components/Comment.js
+++ final-project-frontend/src/components/Comment.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Comment = (props) =>{
 return(
-    <div>
+    <div style={{display:"flex"}}>
         {/* <p>{props.user} said:</p> */}
         <p>{props.commentBody}</p>
         <button onClick={() => props.deleteComment(props.commentId)}>X</button>
