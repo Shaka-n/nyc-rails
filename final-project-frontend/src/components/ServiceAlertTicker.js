1352b470e0e50220fd7d3da3bdc4125829e01991
Garen Sahagian <g.s.sahagian@gmail.com>
2020-07-06 15:26:45 -0400

    Fixed the performance issue. Never setState in a loop, lesson learned.

--- final-project-frontend/src/components/ServiceAlertTicker.js
+++ final-project-frontend/src/components/ServiceAlertTicker.js
@@ -8,10 +8,27 @@ const ServiceAlertTicker = (props) =>{
         {/* style={{animation: "ticker 60s infinite linear"}} */}
             <div className={"ticker__list"} >
                 {props.serviceAlerts.map(alert =>{
-                    if(alert.alert.includes("[L]")){
-                        // style={{maxHeight: "20px", width: "500px"}}
-                        return <div key={alert.id} className={"ticker__item"}>{alert.alert}</div>
+                    switch(props.selectedLine){
+                        case'l':
+                        if(alert.alert.includes("[L]")){
+                            // style={{maxHeight: "20px", width: "500px"}}
+                            return <div key={alert.id} className={"ticker__item"}>{alert.alert}</div>
+                        }
+                        break;
+                        case 'ace':
+                            if(alert.alert.includes("[A]")|| alert.alert.includes("[C]")|| alert.alert.includes("[E]")){
+                                // style={{maxHeight: "20px", width: "500px"}}
+                                return <div key={alert.id} className={"ticker__item"}>{alert.alert}</div>
+                            }
+                        // case '123456':
+                        //     if(alert.alert.includes("[1]")|| alert.alert.includes("[2]")|| alert.alert.includes("[3]")){
+                        //         // style={{maxHeight: "20px", width: "500px"}}
+                        //         return <div key={alert.id} className={"ticker__item"}>{alert.alert}</div>
+                        //     }
+                        default:
+                            return <div key={alert.id} className={"ticker__item"}>{alert.alert}</div>
                     }
+                    
                 })}
             </div>
         </div>    
